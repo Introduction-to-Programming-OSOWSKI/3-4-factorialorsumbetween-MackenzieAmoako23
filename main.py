15ab1c1a2ed9cd21ec4ddc599e607eaa72983ed9
MackenzieAmoako23 <831590@mystma.org>
2021-01-22 16:12:09 +0000

    3-4 factOrSum -1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#WRITE YOUR CODE IN THIS FILE
+def factOrSum(O, x):
+    num = 1
+
+    if O == "factorial":
+        for i in range( 1, x + 1):
+            num = num * i
+        return num
+
+    else:
+        total = 0
+        
+        for i in range(0 , x):
+            total = total + i
+        return total
